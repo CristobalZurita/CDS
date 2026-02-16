2c84f8a94f117d2fa42d1078c7cfd49b93b68efb
Cristóbal Zurita Quintanilla <cristobal.z.q@gmail.com>
2026-02-16 15:07:54 -0300

    fix: Add lang="ts" to script setup for proper TypeScript support in Cotizador

--- src/vue/components/quotation/InteractiveInstrumentDiagnostic.vue
+++ src/vue/components/quotation/InteractiveInstrumentDiagnostic.vue
@@ -457,7 +457,7 @@
   </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import { ref, computed, watch, nextTick } from 'vue'
 import { useInstrumentsCatalog } from '@/composables/useInstrumentsCatalog'
 
